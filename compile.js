99ae4b21a144045e6b6c63eca60cffab95a9e27e
Rayce Coyne <rayce.coyne@gmail.com>
2022-05-05 17:22:47 -0500

    initial push with compile and test contract

---
+++ compile.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts','inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf-8');
+
+const input = {
+    language: 'Solidity',
+    sources: {
+      'inbox.sol': {
+        content: source,
+      },
+    },
+    settings: {
+      outputSelection: {
+        '*': {
+          '*': ['*'],
+        },
+      },
+    },
+  };
+
+module.exports = JSON.parse(solc.compile(JSON.stringify(input))).contracts['inbox.sol'].Inbox;
+
+
---
+++ deploy.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const mneumonic = process.env.META_MNEUMONIC
+const endpoint = process.env.INFURA_RINKEBY_ENDPOINT
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const {abi, evm} = require('./compile');
+
+const provider = new HDWalletProvider(mneumonic, endpoint);
+const web3 = new Web3(provider);
+
+const initialMessage = 'Initial Message'
+
+const deploy = async () =>{
+    const accounts = await web3.eth.getAccounts();
+    console.log('attempting to deploy from account ', accounts[0]);
+    const result = await new web3.eth.Contract(abi)
+        .deploy({data: evm.bytecode.object, arguments:[initialMessage] })
+        .send({gas: '1000000', from: accounts[0]});
+        
+    console.log('contract deployed to ', result.options.address);
+    provider.engine.stop();
+};
+deploy();
